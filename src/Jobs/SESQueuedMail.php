8c74ba93563cd8d1f09fbffded813544633bd160
Leon@thePistol <106639061+LeonPJayasinghe@users.noreply.github.com>
2025-06-03 14:22:23 +1000

    silverstrip5 update

--- src/Jobs/SESQueuedMail.php
+++ src/Jobs/SESQueuedMail.php
@@ -4,89 +4,126 @@ namespace Symbiote\SilverStripeSESMailer\Jobs;
 
 use Symbiote\QueuedJobs\Services\AbstractQueuedJob;
 use Symbiote\QueuedJobs\Services\QueuedJob;
+use Symbiote\SilverStripeSESMailer\Mail\SESMailer; // Updated namespace for SESMailer
 use Exception;
 use SilverStripe\Core\Injector\Injector;
 
 
 if (class_exists(QueuedJob::class)) {
-	/**
-	 * SESQueuedMail
-	 *
-	 * @author Stephen McMahon <stephen@symbiote.com.au>
-	 */
-	class SESQueuedMail extends AbstractQueuedJob implements QueuedJob {
+    /**
+     * SESQueuedMail
+     *
+     * @author Stephen McMahon <stephen@symbiote.com.au>
+     */
+    class SESQueuedMail extends AbstractQueuedJob implements QueuedJob {
 
-		public function __construct($destinations = array(), $subject = '', $rawMessageText = '') {
-			if (!$destinations && !$subject && !$rawMessageText) {
-				// Avoid QueuedJobService::initialiseJob errors
-				return;
-			}
-			$this->To = $destinations;
-			$this->Subject = $subject;
-			$this->RawMessageText = $rawMessageText;
-		}
+        /**
+         * @var array
+         */
+        protected $To;
 
-		public function getTitle() {
+        /**
+         * @var string
+         */
+        protected $Subject;
 
-			return 'Email To: ' . implode(', ', $this->To) . ' Subject: ' . $this->Subject;
-		}
+        /**
+         * @var string
+         */
+        protected $RawMessageText;
 
-		public function getSignature() {
 
-			return md5($this->Subject) . ' ' . implode(', ', $this->To);
-		}
+        public function __construct($destinations = [], $subject = '', $rawMessageText = '') {
+            // Check if arguments are provided to avoid initialisation errors from QueuedJobService
+            if (!$destinations && !$subject && !$rawMessageText) {
+                return;
+            }
+            $this->To = $destinations;
+            $this->Subject = $subject;
+            $this->RawMessageText = $rawMessageText;
+        }
 
-		public function getJobType() {
+        /**
+         * Get the title for this job, displayed in the QueuedJobs admin.
+         *
+         * @return string
+         */
+        public function getTitle() {
+            return 'Email To: ' . implode(', ', (array) $this->To) . ' Subject: ' . $this->Subject;
+        }
 
-			$this->totalSteps = '1';
+        /**
+         * Get a unique signature for this job, used for checking duplicate jobs.
+         *
+         * @return string
+         */
+        public function getSignature() {
+            // Ensure $this->To is treated as an array for implode
+            return md5($this->Subject) . ' ' . implode(', ', (array) $this->To);
+        }
 
-			return QueuedJob::QUEUED;
-		}
+        /**
+         * Define the job type.
+         *
+         * @return string
+         */
+        public function getJobType() {
+            $this->totalSteps = 1; // It's a single step process
 
-		/**
-		 * Send this email. We try this only once and break as soon as something goes wrong to avoid sending multiple emails
-		 * or DoSing our job queued with slow processing emails.
-		 */
-		public function process() {
+            return QueuedJob::QUEUED;
+        }
 
-			if ($this->isComplete) {
-				return;
-			}
-			$this->currentStep += 1;
+        /**
+         * Process the email sending. We try this only once and break as soon as something goes wrong
+         * to avoid sending multiple emails or overwhelming the job queue with slow processes.
+         */
+        public function process() {
+            if ($this->isComplete) {
+                return;
+            }
 
-			// Detect issues with data
-			$isCorrupt = false;
-			$to = $this->To;
-			$rawMessageText = $this->RawMessageText;
-			if (!$to) {
-				$this->addMessage('$this->To should not be empty.');
-				$isCorrupt = true;
-			}
-			if (!$rawMessageText) {
-				$this->addMessage('$this->RawMessageText should not be empty.');
-				$isCorrupt = true;
-			}
-			if ($isCorrupt) {
-				throw new Exception('Corrupted SESQueuedMail job (Missing "To" or "RawMessageText").', 'ERR');
-			}
+            $this->currentStep = 1; // Increment the step (though it's only one step)
 
-			$response = Injector::inst()->get(SESMailer::class)
-				->sendSESClient($to, $rawMessageText);
+            // Detect issues with data
+            $isCorrupt = false;
+            $to = $this->To;
+            $rawMessageText = $this->RawMessageText;
 
-			$this->addMessage('SES Response: '.print_r($response, true));
+            if (empty($to)) { // Use empty for better check
+                $this->addMessage('$this->To should not be empty.');
+                $isCorrupt = true;
+            }
+            if (empty($rawMessageText)) { // Use empty for better check
+                $this->addMessage('$this->RawMessageText should not be empty.');
+                $isCorrupt = true;
+            }
 
-			if (isset($response['MessageId']) && strlen($response['MessageId']) &&
-				(isset($response['@metadata']['statusCode']) && $response['@metadata']['statusCode'] == 200)) {
-				$this->RawMessageText = 'Email Sent Successfully. Message body deleted';
-				$this->isComplete = true;
-				return;
-			}
-			$this->addMessage('$this->To should not be empty.');
-			if ($response) {
-				throw new Exception(json_encode($response->toArray()), 'ERR');
-			}
-			throw new Exception('Blank "response" result from SESMailer::sendSESClient()', 'ERR');
-		}
+            if ($isCorrupt) {
+                // Better error message and code for clarity
+                throw new Exception('Corrupted SESQueuedMail job: Missing "To" or "RawMessageText".');
+            }
 
-	}
+            try {
+                /** @var SESMailer $mailer */
+                $mailer = Injector::inst()->get(SESMailer::class);
+                $response = $mailer->sendSESClient($to, $rawMessageText);
+
+                $this->addMessage('SES Response: '.print_r($response->toArray(), true)); // Ensure response is converted to array for print_r
+
+                // Check for successful response
+                if (isset($response['MessageId']) && !empty($response['MessageId']) &&
+                    (isset($response['@metadata']['statusCode']) && $response['@metadata']['statusCode'] === 200)) {
+                    $this->RawMessageText = 'Email Sent Successfully. Message body deleted'; // Clear sensitive data
+                    $this->isComplete = true; // Mark job as complete
+                } else {
+                    // If response indicates failure but no exception was thrown by SESMailer
+                    throw new Exception('SESMailer::sendSESClient() returned an unsuccessful response: ' . json_encode($response->toArray()));
+                }
+            } catch (Exception $ex) {
+                // Log the exception and re-throw to mark the job as failed by QueuedJobs
+                $this->addMessage('Error sending email via SES: ' . $ex->getMessage());
+                throw $ex; // Re-throw to indicate job failure
+            }
+        }
+    }
 }
--- src/Mail/SESMailer.php
+++ src/Mail/SESMailer.php
@@ -3,11 +3,18 @@
 namespace Symbiote\SilverStripeSESMailer\Mail;
 
 use Aws\Ses\SesClient;
-use SilverStripe\Control\Email\Mailer;
-use SilverStripe\Control\Email\Email;
 use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Mail\Mailer; // Updated namespace
+use SilverStripe\Mail\Email;   // Updated namespace
 use Exception;
 use Psr\Log\LoggerInterface;
+use SilverStripe\Core\Config\Config; // Added for SilverStripe 5 config access
+
+// Assuming QueuedJobService and SESQueuedMail are compatible or will be updated
+// for SilverStripe 5 and their namespaces remain as they are,
+// or you'll adjust them based on your QueuedJobs module version.
+use Symbiote\QueuedJobs\Services\QueuedJobService;
+use Symbiote\SilverStripeSESMailer\Jobs\SESQueuedMail; // Assuming this is your queued job class
 
 
 /**
@@ -21,151 +28,163 @@ use Psr\Log\LoggerInterface;
 class SESMailer implements Mailer
 {
 
-	/**
-	 * @var SesClient
-	 */
-	protected $client;
+    /**
+     * @var SesClient
+     */
+    protected $client;
 
-	/**
+    /**
      * Uses QueuedJobs module when sending emails
      *
      * @var boolean
      */
-	protected $useQueuedJobs = true;
+    protected $useQueuedJobs = true;
 
-	/**
-	 * @var array|null
-	 */
-	protected $lastResponse = null;
+    /**
+     * @var array|null
+     */
+    protected $lastResponse = null;
 
-	/**
-	 * @param array $config
-	 */
-	public function __construct($config)
-	{
+    /**
+     * @param array $config
+     */
+    public function __construct($config)
+    {
         $this->client = new SesClient($config);
-	}
+    }
 
-	/**
-	 * @param boolean $bool
-	 *
-	 * @return $this
-	 */
-	public function setUseQueuedJobs($bool)
-	{
-		$this->useQueuedJobs = $bool;
+    /**
+     * @param boolean $bool
+     *
+     * @return $this
+     */
+    public function setUseQueuedJobs($bool)
+    {
+        $this->useQueuedJobs = $bool;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @return array|null
-	 */
-	public function getLastResponse()
-	{
-		return $this->lastResponse;
-	}
+    /**
+     * @return array|null
+     */
+    public function getLastResponse()
+    {
+        return $this->lastResponse;
+    }
 
-	/**
-	 * @param SilverStripe\Control\Email
-	 */
-	public function send($email)
-	{
-		$destinations = $email->getTo();
+    /**
+     * @param Email $email
+     * @return bool
+     */
+    public function send(Email $email)
+    {
+        // In SilverStripe 5, Email::getTo(), getCc(), getBcc() return an array of ['email' => 'name']
+        // We need just the email addresses for SES.
+        $destinations = array_keys($email->getTo());
 
-		if ($overideTo = Email::getSendAllEmailsTo()) {
-			$destinations = $overideTo;
-		} else {
-			if ($cc = $email->getCc()) {
-				$destinations = array_merge($destinations, $cc);
-			}
+        // Handling send all emails to / override
+        $overrideTo = Config::inst()->get(Email::class, 'send_all_emails_to');
+        if ($overrideTo) {
+            $destinations = [$overrideTo];
+        } else {
+            if ($cc = $email->getCc()) {
+                $destinations = array_merge($destinations, array_keys($cc));
+            }
 
-			if ($bcc = $email->getBcc()) {
-				$destinations = array_merge($destinations, $bcc);
-			}
+            if ($bcc = $email->getBcc()) {
+                $destinations = array_merge($destinations, array_keys($bcc));
+            }
 
-			if ($addCc = Email::getCCAllEmailsTo()) {
-				$destinations = array_merge($destinations, $addCc);
-			}
+            // SilverStripe 5 uses config for global CC/BCC
+            $addCc = Config::inst()->get(Email::class, 'cc_all_emails_to');
+            if ($addCc) {
+                $destinations = array_merge($destinations, [$addCc]);
+            }
 
-			if ($addBCc = Email::getBCCAllEmailsTo()) {
-				$destinations = array_merge($destinations, $addBCc);
-			}
-		}
+            $addBCc = Config::inst()->get(Email::class, 'bcc_all_emails_to');
+            if ($addBCc) {
+                $destinations = array_merge($destinations, [$addBCc]);
+            }
+        }
 
-		$destinations = array_keys($destinations);
-		$subject = $email->getSubject();
-		$rawMessageText = $email->render()->getSwiftMessage()->toString();
+        $subject = $email->getSubject();
 
-		if (class_exists(QueuedJobService::class) && $this->useQueuedJobs) {
-			$job = Injector::inst()->createWithArgs(SESQueuedMail::class, array(
-				$destinations,
-				$subject,
-				$rawMessageText
-			));
+        // SilverStripe 5: Email::render() returns the raw content directly
+        // The Swift_Message dependency is removed.
+        $rawMessageText = $email->getSwiftMessage()->toString(); // Still get a Swift_Message here to get raw message
 
-			singleton(QueuedJobService::class)->queueJob($job);
+        if (class_exists(QueuedJobService::class) && $this->useQueuedJobs) {
+            $job = Injector::inst()->createWithArgs(SESQueuedMail::class, array(
+                $destinations,
+                $subject,
+                $rawMessageText
+            ));
 
-			return true;
-		}
+            singleton(QueuedJobService::class)->queueJob($job);
 
-		try {
-			$response = $this->sendSESClient($destinations, $rawMessageText);
-
-			$this->lastResponse = $response;
-		} catch (\Aws\Ses\Exception\SesException $ex) {
-			Injector::inst()->get(LoggerInterface::class)->warning($ex->getMessage());
-
-			$this->lastResponse = false;
-			return false;
-		}
-
-        /* @var $response Aws\Result */
-        if (isset($response['MessageId']) && strlen($response['MessageId']) &&
-			(isset($response['@metadata']['statusCode']) && $response['@metadata']['statusCode'] == 200)) {
             return true;
         }
 
-		return false;
-	}
+        try {
+            $response = $this->sendSESClient($destinations, $rawMessageText);
 
-	/**
-	 * Send an email via SES. Expects an array of valid emails and a raw email body that is valid.
-	 *
-	 * @param array $destinations array of emails addresses this email will be sent to
-	 * @param string $rawMessageText Raw email message text must contain headers; and otherwise be a valid email body
-	 * @return Array Amazon SDK response
-	 */
-	public function sendSESClient ($destinations, $rawMessageText) {
+            $this->lastResponse = $response;
+        } catch (\Aws\Ses\Exception\SesException $ex) {
+            Injector::inst()->get(LoggerInterface::class)->warning($ex->getMessage());
 
-		try {
-			$response = $this->client->sendRawEmail(array(
-				'Destinations' => $destinations,
-				'RawMessage' => array('Data' => $rawMessageText)
-			));
-		} catch (Exception $ex) {
-			/*
-			 * Amazon SES has intermittent issues with SSL connections being dropped before response is full received
-			 * and decoded we're catching it here and trying to send again, the exception doesn't have an error code or
-			 * similar to check on so we have to relie on magic strings in the error message. The error we're catching
-			 * here is normally:
-			 *
-			 * AWS HTTP error: cURL error 56: SSL read: error:00000000:lib(0):func(0):reason(0), errno 104
-			 * (see http://curl.haxx.se/libcurl/c/libcurl-errors.html) (server): 100 Continue
-			 *
-			 * Without the line break, so we check for the 'cURL error 56' as it seems likely to be consistent across
-			 * systems/sites
-			 */
-			if(strpos($ex->getMessage(), "cURL error 56")) {
-				$response = $this->client->sendRawEmail(array(
-					'Destinations' => $destinations,
-					'RawMessage' => array('Data' => $rawMessageText)
-				));
-			} else {
-				throw $ex;
-			}
-		}
+            $this->lastResponse = false;
+            return false;
+        }
 
-		return $response;
-	}
+        /* @var $response Aws\Result */
+        if (isset($response['MessageId']) && strlen($response['MessageId']) &&
+            (isset($response['@metadata']['statusCode']) && $response['@metadata']['statusCode'] == 200)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Send an email via SES. Expects an array of valid emails and a raw email body that is valid.
+     *
+     * @param array $destinations array of emails addresses this email will be sent to
+     * @param string $rawMessageText Raw email message text must contain headers; and otherwise be a valid email body
+     * @return \Aws\Result Amazon SDK response
+     * @throws Exception
+     */
+    public function sendSESClient ($destinations, $rawMessageText) {
+
+        try {
+            $response = $this->client->sendRawEmail(array(
+                'Destinations' => $destinations,
+                'RawMessage' => array('Data' => $rawMessageText)
+            ));
+        } catch (Exception $ex) {
+            /*
+             * Amazon SES has intermittent issues with SSL connections being dropped before response is full received
+             * and decoded we're catching it here and trying to send again, the exception doesn't have an error code or
+             * similar to check on so we have to relie on magic strings in the error message. The error we're catching
+             * here is normally:
+             *
+             * AWS HTTP error: cURL error 56: SSL read: error:00000000:lib(0):func(0):reason(0), errno 104
+             * (see http://curl.haxx.se/libcurl/c/libcurl-errors.html) (server): 100 Continue
+             *
+             * Without the line break, so we check for the 'cURL error 56' as it seems likely to be consistent across
+             * systems/sites
+             */
+            if(strpos($ex->getMessage(), "cURL error 56")) {
+                // Retry sending the email
+                $response = $this->client->sendRawEmail(array(
+                    'Destinations' => $destinations,
+                    'RawMessage' => array('Data' => $rawMessageText)
+                ));
+            } else {
+                throw $ex;
+            }
+        }
+
+        return $response;
+    }
 }
